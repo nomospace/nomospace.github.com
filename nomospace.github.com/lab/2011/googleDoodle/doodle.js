80c3314e65fa4a667df44d443f7e3f575970c1be
nomospace <jinlu_hz@163.com>
2011-05-24 15:33:08 +0800

    update doodle demo

--- nomospace.github.com/lab/2011/googleDoodle/doodle.js
+++ nomospace.github.com/lab/2011/googleDoodle/doodle.js
@@ -9,12 +9,12 @@ define(function(require){
 		_html, 
 		_style,
 		_node = document.getElementById('doodle'),
-		_data = require('./google_doodle_data').getDoodleMapData(), 
+		_data = require('./data').getDoodleMapData(), 
 		_func = function(){
 	        if (_style = _data.shift()) {
 	            _html = '<div style="' + 'position:absolute;' +
 		            'left:' + _style[0] + 'px;top:' + _style[1] + 'px;width:' + _style[2] + 'px;height:' + _style[3] +
-		            'px;background:url(o_graham11-hp-sprite.png) no-repeat ' + _style[4] + 'px ' + _style[5] + 'px;"></div>';
+		            'px;background:url(sprite.png) no-repeat ' + _style[4] + 'px ' + _style[5] + 'px;"></div>';
 	            _tmp = document.createElement('div');
 	            _tmp.innerHTML = _html;
 	            _node.appendChild(_tmp.childNodes[0]);
