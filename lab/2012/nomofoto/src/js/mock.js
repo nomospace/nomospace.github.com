42790d442cf5ccf101ab64ef89b456702fb32c32
nomospace <jinlu_hz@163.com>
2012-06-04 17:11:57 +0800

    add nomofoto module ..

---
+++ lab/2012/nomofoto/src/js/mock.js
@@ -0,0 +1,3 @@
+$.ajax({
+    url: 'https://github.com/nomospace/nomospace.github.com/cache/commits/3739e276a3229b0a6e2dd2c78eb3c4ed8742dd53?commit_sha=dfd421c7385fb92f32e2b73573857a30fa92b1bc&path=_site&_=1338799224452'
+})
