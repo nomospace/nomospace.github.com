b204e1a692f57ebadad173b47ba9c6b64110abe4
nomospace <jinlu_hz@163.com>
2012-08-20 22:06:48 +0800

    填充 douban 数据

--- _site/assets/js/douban.js
+++ _site/assets/js/douban.js
@@ -15,8 +15,90 @@ $(function() {
       cat: 'book',
       maxresults: '100',
       callback: function(books) {
-        console.log(books);
+        function transform(object) {
+          for (var i in object) {
+            if (!i) continue;
+
+            var key;
+            if (i.indexOf('@') != -1) {
+              key = i.split('@').join('');
+            } else if (i.indexOf(':') != -1) {
+              key = i.split(':').join('');
+            } else if (i.indexOf('$') != -1) {
+              key = i.split('$').join('');
+            }
+
+            if (key && i) {
+              if (!object[key] && object[i]) object[key] = object[i];
+            }
+
+            if ($.isPlainObject(object[i]) || $.isArray(object[i])) {
+              transform(object[i]);
+            }
+
+          }
+        }
+
+        transform(books);
+
+        // console.log(books);
         var template = Handlebars.compile(tpl);
+
+        Handlebars.registerHelper('image', function() {
+          var result = '';
+          $.each(this.link, function(i, r) {
+            if (r.rel == 'image') {
+              result = r.href;
+              return false;
+            }
+          });
+          return result;
+        });
+
+        Handlebars.registerHelper('href', function() {
+          var result = '';
+          $.each(this.link, function(i, r) {
+            if (r.rel == 'alternate') {
+              result = r.href;
+              return false;
+            }
+          });
+          return result;
+        });
+
+        Handlebars.registerHelper('publisher', function() {
+          var result = '';
+          $.each(this.dbattribute, function(i, r) {
+            if (r.name == 'publisher') {
+              result = r.t;
+              return false;
+            }
+          });
+          return result;
+        });
+
+        Handlebars.registerHelper('price', function() {
+          var result = '';
+          $.each(this.dbattribute, function(i, r) {
+            if (r.name == 'price') {
+              result = r.t;
+              return false;
+            }
+          });
+          return result;
+        });
+
+        Handlebars.registerHelper('pubdate', function() {
+          var result = '';
+          $.each(this.dbattribute, function(i, r) {
+            if (r.name == 'pubdate') {
+              result = r.t;
+              return false;
+            }
+          });
+          return result;
+        });
+
         $books.html(template(books));
 
         // var source = "<p>Hello, my name is {{name}}. I am from {{hometown}}. I have " + "{{kids.length}} kids:</p>" + "<ul>{{#kids}}<li>{{name}} is {{age}}</li>{{/kids}}</ul>";
