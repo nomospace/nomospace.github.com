d3a8d2bef274b9026be2fbe404dcef2ff62b11ec
nomospace <jinlu_hz@163.com>
2012-10-02 12:13:54 +0800

    add weibo_code

--- assets/js/site.js
+++ assets/js/site.js
@@ -2,4 +2,10 @@ $(function() {
   $('pre').each(function() {
     hljs && hljs.highlightBlock(this);
   });
+  // weibo code
+  var search = location.search,
+    code = 'weibo_code';
+  if (search && search.slice(1, [code].length) == code) {
+    location.replace('http://localhost:3002/api' + code + '/' + search.slice(1));
+  }
 });
