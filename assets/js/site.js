ff5f70628cbb80345ca9d531b87fa2795f5287dc
nomospace <jinlu_hz@163.com>
2012-10-02 12:20:20 +0800

    update weibo_code

--- assets/js/site.js
+++ assets/js/site.js
@@ -5,7 +5,7 @@ $(function() {
   // weibo code
   var search = location.search,
     code = 'code';
-  if (search && search.slice(1, [code].length) == code) {
+  if (search && search.slice(1, code.length + 1) == code) {
     location.replace('http://localhost:3002/api' + code + '/' + search.slice(1));
   }
 });
