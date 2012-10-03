800e9a116ef94a58448f1213be1bb84e45b67457
nomospace <jinlu_hz@163.com>
2012-10-03 19:08:54 +0800

    update weibo_code

--- assets/js/site.js
+++ assets/js/site.js
@@ -2,13 +2,4 @@ $(function() {
   $('pre').each(function() {
     hljs && hljs.highlightBlock(this);
   });
-  // weibo code
-  var search = location.search,
-    code = 'code', length = code.length;
-  if (search) {
-    var _code = search.slice(1, length + 1);
-    if (_code == code) {
-      location.replace('http://localhost:3002/api/' + code + '/' + search.slice(length + 2));
-    }
-  }
 });
