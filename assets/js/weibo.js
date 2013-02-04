d4e547e04bce09a1d928446684abe4a8544bedab
nomospace <jinlu_hz@163.com>
2013-02-05 00:46:07 +0800

    Update repo

--- assets/js/weibo.js
+++ assets/js/weibo.js
@@ -1,9 +1,8 @@
 (function() {
   var param = $.url().param(),
-    type = param.type,
     code = param.code;
   if (type && code) {
-    console.log('http://localhost:3002/api/' + type + '/' + code);
-    location.replace('http://localhost:3002/api/' + type + '/' + code);
+//    console.log('http://localhost:3002/api/' + type + '/code/' + code);
+    location.replace('http://localhost:3002/api/' + type + '/code' + code);
   }
 })();
