4dc88ae12a654ee7147f0e5e63673f6852f24472
nomospace <jinlu_hz@163.com>
2013-02-05 10:50:40 +0800

    Update repo

--- _plugins/sitemap_generator.rb
+++ _plugins/sitemap_generator.rb
@@ -39,7 +39,7 @@ require 'rexml/document'
 module Jekyll
 
   # Change MY_URL to reflect the site you are using
-  MY_URL = "http://elfjs.com"
+  MY_URL = "http://nomospace.github.com"
 
   # Change SITEMAP_FILE_NAME if you would like your sitemap file
   # to be called something else
--- assets/js/weibo.js
+++ assets/js/weibo.js
@@ -3,6 +3,6 @@
     code = param.code;
   if (type && code) {
 //    console.log('http://localhost:3002/api/' + type + '/code/' + code);
-    location.replace('http://localhost:3002/api/' + type + '/code' + code);
+    location.replace('http://localhost:3002/api/' + type + '/code/' + code);
   }
 })();
