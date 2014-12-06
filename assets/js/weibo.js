5724f0ad853366fe03229bab9236877511585ec3
nomospace <jinlu_hz@163.com>
2014-12-06 23:07:42 +0800

    Update OAuth2.0 callback url

--- assets/js/weibo.js
+++ assets/js/weibo.js
@@ -1,11 +1,12 @@
 (function() {
   var param = $.url().param(),
     code = param.code, url;
+  var referrer = document.referrer.slice(0, -6);
   if (type && code) {
     if (type == 'qq') {
-      url = 'http://localhost:3002/api/' + type + '/code/' + location.search.slice(1);
+      url = referrer + 'api/' + type + '/code/' + location.search.slice(1);
     } else if (type == 'sina') {
-      url = 'http://localhost:3002/api/' + type + '/code/' + code;
+      url = referrer + 'api/' + type + '/code/' + code;
     }
     location.replace(url);
   }
